128c6015df246861a928c6a4d4ce2a068688776f
Tobechi Duru <tobechi.duru001@gmail.com>
2023-09-07 10:50:01 -0700

    Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main -Entry point
+ *
+ * a programme that prints standard output
+ *
+ * Return: Always 0 (success)
+ *
+ */
+int main(void)
+{
+	printf("with proper grammer, but the outcome is a piece of art,\n");
+	return (0);
+}
