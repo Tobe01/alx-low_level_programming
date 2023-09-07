661b83758a5e5496dde84b0d03375d0162fe4bc0
Tobechi Duru <tobechi.duru001@gmail.com>
2023-09-07 10:35:30 -0700

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    //Use the puts function to print the text
+    puts("Programming is like building a multilingual puzzle");
+
+    // Return 0 to indicate successful program execution
+    return 0;
+}
