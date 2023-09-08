f9ab7ca6af51eabf71db9b20786946a0badfa500
Tobechi Duru <tobechi.duru001@gmail.com>
2023-09-08 14:17:16 -0700

    inventing

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Description: prints all possible different combinations of two digits.
+ *
+ * Return: 0
+ *
+ */
+int main(void)
+{
+int i = 48;
+while (i < 58)
+{
+int j = i + i;
+while (j < 58)
+{
+putchar(i);
+putchar(j);
+if (i != 56)
+{
+putchar(',');
+putchar(' ');
+}
+j++;
+}
+i++;
+}
+putchar('\n');
+return (0);
+}
