3c82107d6888d3aeb3402bde6c4f4c0bf73c9acf
Tobechi Duru <tobechi.duru001@gmail.com>
2023-09-08 11:14:38 -0700

    alphabet pay off

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ *Description: prints letters except q and e
+ *
+ *Return: Always 0
+ */
+int main(void)
+{
+int i = 97;
+while (i < 123)
+{
+if (i != 101 && i != 113)
+putchar(i);
+i++;
+}
+putchar('\n');
+return (0);
+}
