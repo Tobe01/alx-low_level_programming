55f1268eea8e569e3d344fb8a21480913c678f03
Tobechi Duru <tobechi.duru001@gmail.com>
2023-09-12 17:59:16 -0700

    98 Street

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_to_98 - prints n to 98
+ * @n: The number to start from
+ *
+ * Return: void.
+ */
+void print_to_98(int n)
+{
+if (n < 99)
+while (n < 99)
+{
+printf("%d", n);
+if (n != 98)
+printf(", ");
+n++;
+}
+else
+{
+while (n != 97)
+{
+printf("%d", n);
+if (n != 98)
+printf(", ");
+n--;
+}
+}
+printf("\n");
+}
