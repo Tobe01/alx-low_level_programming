2f2dbfea20174bfb962bbe0e24a824bca24c8746
Tobechi Duru <tobechi.duru001@gmail.com>
2023-09-12 17:02:31 -0700

    10x Alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - prints 10 times the alphabet, in lowercase
+ *
+ * Return: void.
+ */
+void print_alphabet_x10(void)
+{
+int i;
+for (i = 0; i < 10; i++)
+{
+int j;
+for (j = 97; j < 123; j++)
+_putchar(j);
+_putchar('\n');
+}
+}
+
+
+
