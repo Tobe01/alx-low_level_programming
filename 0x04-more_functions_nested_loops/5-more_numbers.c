8b836fab678818b737951e27526e86d1895fffcd
Tobechi Duru <tobechi.duru001@gmail.com>
2023-09-14 19:09:55 -0700

    Universal language

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,26 +1,23 @@
 i#include "main.h"
-
 /**
  * more_numbers - prints 10 times the nnumbers, from 0 to 14.
  *
  * Return: void.
  */
-
 void more_numbers(void)
 {
-	int i, j, k;
-
-	for (i = 0; i < 10; i++)
-	{
-		for (j = 0; j < 15; j++)
-		{
-			k = j;
-			if (j < 10)
-				k *= 10;
-			_putchar((k / 10) + '0');
-			if (j > 9)
-				_putchar((j % 10) + '0');
-		}
-		_putchar('\n');
-	}
+int i, j, k;
+for (i = 0; i < 10; i++)
+{
+for (j = 0; j < 15; j++)
+{
+k = j;
+if (j < 10)
+k *= 10;
+_putchar((k / 10) + '0');
+if (j > 9)
+_putchar((j % 10) + '0');
+}
+_putchar('\n');
+}
 }
