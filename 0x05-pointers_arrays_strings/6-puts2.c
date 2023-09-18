4d1ca3305a5a7f4b12f915623c60056b7294b7dc
Tobechi Duru <tobechi.duru001@gmail.com>
2023-09-18 02:57:44 -0700

    Half the lies

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * puts2 - prints every other character of a string.
+ * @str: string parameter
+ * Return: void
+ */
+
+void puts2(char *str)
+{
+	int len = 0, i = 0;
+
+	while (str[len] != '\0')
+		len++;
+
+	for (; i < len; i += 2)
+		_putchar(str[i]);
+	_putchar('\n');
+}
