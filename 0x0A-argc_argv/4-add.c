6e5909921f0bc2a5fc06335ae1f8996248b5bdc8
Tobechi Duru <tobechi.duru001@gmail.com>
2023-10-02 17:22:21 -0700

    Infinity

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,34 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
-
 /**
- *  * main - adds positive numbers
- *   * @argc: args count
- *    * @argv: string array
- *     * Return: 0
- *      */
-
+ * main - Prints the addition of positive numbers,
+ *        followed by a new line.
+ * @argc: The number of arguments passed to the program.
+ * @argv: An array of pointers to the arguments.
+ *
+ * Return: If one of the numbers contains symbols that are non-digits - 1.
+ *         Otherwise - 0.
+ */
 int main(int argc, char *argv[])
 {
-		int total = 0, i = 1;
-
-			if (argc == 1)
-						puts("0");
-				else
-						{
-									for (; i < argc; i++)
-												{
-																char *e;
-																			int num = strtol(argv[i], &e, 10);
-
-																						if (*e != '\0')
-																										{
-																															puts("Error");
-																																			return (1);
-																																						}
-																									total += num;
-																											}
-											printf("%d\n", total);
-												}
-					return (0);
+int num, digit, sum = 0;
+for (num = 1; num < argc; num++)
+{
+for (digit = 0; argv[num][digit]; digit++)
+{
+if (argv[num][digit] < '0' || argv[num][digit] > '9')
+{
+printf("Error\n");
+return (1);
+}
+}
+sum += atoi(argv[num]);
+}
+printf("%d\n", sum);
+return (0);
 }
