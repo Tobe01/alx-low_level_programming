50114a7e07926829b3bcda8bc0f3baaf02f94a9b
Tobechi Duru <tobechi.duru001@gmail.com>
2023-10-08 18:04:53 -0700

    mully

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,7 +1,6 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
-
 /**
  * main - prints all arguments received.
  * @argc: type int argument
@@ -10,16 +9,15 @@
  */
 int main(int argc, char *argv[])
 {
-	int n1, n2, mul;
-
-	if (argc != 3)
-		printf("Error\n");
-	else
-	{
-		n1 = atoi(argv[1]);
-		n2 = atoi(argv[2]);
-		mul = n1 * n2;
-		printf("%d\n", mul);
-	}
-	return (0);
+int n1, n2, mul;
+if (argc != 3)
+printf("Error\n");
+else
+{
+n1 = atoi(argv[1]);
+n2 = atoi(argv[2]);
+mul = n1 * n2;
+printf("%d\n", mul);
+}
+return (0);
 }
