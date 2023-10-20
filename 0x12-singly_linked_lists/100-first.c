206bf10d95e5b1a414f543ad6456e08b615c8488
Tobechi Duru <tobechi.duru001@gmail.com>
2023-10-19 17:29:18 -0700

    The Hare

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,9 @@
+#include "lists.h"
+/**
+ * before_main - prints a message
+ * Return: void
+ */
+void before_main(void)
+{
+P("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
