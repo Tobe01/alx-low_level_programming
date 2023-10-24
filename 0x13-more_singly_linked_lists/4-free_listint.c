049c7bab96ecebd4243927509fa7cee1c65eb265
Tobechi Duru <tobechi.duru001@gmail.com>
2023-10-23 18:02:07 -0700

    Free list

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+/**
+ * free_listint - frees a list
+ * @head: head of the linked list.
+ * Return: no return.
+ */
+void free_listint(listint_t *head)
+{
+listint_t *current;
+while (head != NULL)
+{
+current = head;
+head = head->next;
+free(current);
+}
+}
