a5948113e6f6f88bbde9adb24172600ab14067e0
Tobechi Duru <tobechi.duru001@gmail.com>
2023-12-22 04:38:51 -0800

    Updates

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -1,16 +1,16 @@
 #include "hash_tables.h"
 /**
  * hash_djb2 - Hash function implementing the djb2 algorithm.
- * @str: String to be hashed.
- * Return: Hashed string.
+ * @str: The string to hash.
+ *
+ * Return: The calculated hash.
  */
 unsigned long int hash_djb2(const unsigned char *str)
 {
-unsigned long int hash = 5381;
+unsigned long int hash;
 int c;
+hash = 5381;
 while ((c = *str++))
-{
 hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
-}
 return (hash);
 }
